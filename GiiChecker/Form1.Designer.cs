da6e5d11a16ca45b748aab82e4ef195b51f62b7e
lleldigm <lleldigm@gmail.com>
2020-05-31 02:13:04 +0900

    init commit

---
+++ GiiChecker/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GiiChecker
+{
+    partial class itemInfoLuaFixTool
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.srcButton = new System.Windows.Forms.Button();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.fixButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "itemInfo.luaのパス";
+            // 
+            // srcButton
+            // 
+            this.srcButton.Location = new System.Drawing.Point(108, 10);
+            this.srcButton.Name = "srcButton";
+            this.srcButton.Size = new System.Drawing.Size(44, 23);
+            this.srcButton.TabIndex = 1;
+            this.srcButton.Text = "参照";
+            this.srcButton.UseVisualStyleBackColor = true;
+            this.srcButton.Click += new System.EventHandler(this.srcButton_Click);
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(160, 12);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.ReadOnly = true;
+            this.pathTextBox.Size = new System.Drawing.Size(201, 19);
+            this.pathTextBox.TabIndex = 2;
+            // 
+            // fixButton
+            // 
+            this.fixButton.Location = new System.Drawing.Point(144, 39);
+            this.fixButton.Name = "fixButton";
+            this.fixButton.Size = new System.Drawing.Size(75, 23);
+            this.fixButton.TabIndex = 3;
+            this.fixButton.Text = "修正する";
+            this.fixButton.UseVisualStyleBackColor = true;
+            this.fixButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // itemInfoLuaFixTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 67);
+            this.Controls.Add(this.fixButton);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.srcButton);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "itemInfoLuaFixTool";
+            this.Text = "GiiChecker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button srcButton;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button fixButton;
+    }
+}
+
---
+++ GiiChecker/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace GiiChecker
+{
+    public partial class itemInfoLuaFixTool : Form
+    {
+        public itemInfoLuaFixTool()
+        {
+            InitializeComponent();
+        }
+
+
+        //参照ボタン
+        private void srcButton_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            ofd.FileName = "";
+            //はじめに表示されるフォルダを指定する
+
+            ofd.InitialDirectory = @"C:";
+            //[ファイルの種類]に表示される選択肢を指定する
+            ofd.Filter = "itemInfo.luaファイル(*.lua)|*.lua|すべてのファイル(*.*)|*.*";
+
+            //フィルターにluaが選択されている状態にする
+            ofd.FilterIndex = 1;
+
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //okが押された際パスをtextへ挿入
+                pathTextBox.Text = ofd.FileName;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+
+
+        private async Task fileCheck()
+        {
+            
+
+            var task = Task.Run(() =>
+            {
+                // textBox2.Focus()の実行
+
+
+                //インプット
+                StreamReader sr = new StreamReader(
+                    pathTextBox.Text,
+                    System.Text.Encoding.GetEncoding("shift_jis")
+                    );
+
+                //アウトプット(itemInfo.lua_fixと名前なる)
+
+                string fileName = pathTextBox.Text.Replace(".lua", "_fix.lua");
+                
+                StreamWriter sw =
+                    new StreamWriter(
+                        fileName,
+                        false,
+                        System.Text.Encoding.GetEncoding("shift_jis")
+                        );
+
+
+                bool endFlag = false;
+
+                while (sr.Peek() >= 0)
+                {
+
+                    
+
+                    
+                    //1行取り出す
+                    string text = sr.ReadLine();
+
+                    if (text == "--Function #0")
+                    {
+                        Console.WriteLine("aaaaaaaaaaaaaaaaaaa");
+                        endFlag = true;
+                    }
+
+
+
+                    //修正①(",",　→   ",)に変換
+                    text = text.Replace("\",\",", "\",");
+
+
+
+
+
+                    //修正②(",",　→   ",)に変換
+
+
+                    char[] cArray = text.ToCharArray();
+                    StringBuilder sb = new StringBuilder();
+
+                    int count = 0;
+                    bool fastFlag = false;
+                    foreach (char c in cArray)
+                    {
+                        //1回目の"がでてくるまではappend
+
+                        if (fastFlag == true && c == '"' && (count != (cArray.Length - 2)) && (endFlag == false))
+                        {
+
+                            sb.Append("\\");
+                            sb.Append(c);
+
+                        }
+                        else
+                        {
+                            sb.Append(c);
+                        }
+
+                        if (c == '"')
+                        {
+
+                            fastFlag = true;
+
+                        }
+
+                        //1回目が出てきたらflag_on
+                        //最後のlength-1でflag無視
+
+
+                        count++;
+
+                    }
+
+
+                    sw.WriteLine(sb.ToString());
+
+
+                }
+ 
+
+                sr.Close();
+                sw.Close();
+
+                MessageBox.Show("修正完了");
+                
+            });
+        }
+
+        //修正処理
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            
+            //luaファイル選択時実行
+            if (pathTextBox.Text.EndsWith("lua"))
+            {
+                var Task = fileCheck();
+                
+
+            }
+            else
+            {
+                MessageBox.Show("luaファイルを指定してください");
+
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
